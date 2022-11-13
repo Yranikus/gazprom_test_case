fcc92c1b0bab6ae1948b77ff5b2fa8ef5adc9e7b
yranikus <kola.kryt@gmail.com>
2022-11-13 22:38:40 +0300

    micro fix

--- src/main/java/com/example/gazprom_test_case/controllers/MembershipController.java
+++ src/main/java/com/example/gazprom_test_case/controllers/MembershipController.java
@@ -3,6 +3,7 @@ package com.example.gazprom_test_case.controllers;
 import com.example.gazprom_test_case.entities.RequestEntity;
 import com.example.gazprom_test_case.service.MembrshipCheckService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController("/")
@@ -13,7 +14,7 @@ public class MembershipController {
 
 
     @PostMapping(value = "/{vk_service_token}" )
-    public Object checkMembership(@RequestBody RequestEntity requestEntity, @PathVariable String vk_service_token) {
+    public ResponseEntity checkMembership(@RequestBody RequestEntity requestEntity, @PathVariable String vk_service_token) {
         return validationService.checkMembership(requestEntity.getUser_id(),requestEntity.getGroup_id(),vk_service_token);
     }
 
